d3c18b55953964e83945d36f449232ca048c18f1
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-27 11:39:22 -0700

    added scripts to load and reset data (small data files only containing 20 lines)

--- reset_small.sql
+++ reset_small.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS answers_photos;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS questions;
+
 CREATE TABLE IF NOT EXISTS questions (
   id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   product_id integer NOT NULL,
@@ -25,3 +29,18 @@ CREATE TABLE IF NOT EXISTS answers_photos (
   answer_id integer REFERENCES answers(id) NOT NULL,
   url text NOT NULL
 );
+
+COPY questions
+FROM '/Users/home/Hack Reactor/Immersive/SDC/QuestionsAndAnswersService/data/small/questions.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY answers
+FROM '/Users/home/Hack Reactor/Immersive/SDC/QuestionsAndAnswersService/data/small/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY answers_photos
+FROM '/Users/home/Hack Reactor/Immersive/SDC/QuestionsAndAnswersService/data/small/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
