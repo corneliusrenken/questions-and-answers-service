c2f1aa6629bea0b53ccf3dc13f26e038b1804a2a
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-27 14:43:26 -0700

    fixed not dividing by 1000

--- scripts/PostgreSQL/unix_to_date.sql
+++ scripts/PostgreSQL/unix_to_date.sql
@@ -1,3 +1,7 @@
 ALTER TABLE questions
 ALTER COLUMN created_at TYPE timestamp
-USING TO_TIMESTAMP(created_at);
+USING TO_TIMESTAMP(created_at / 1000);
+
+ALTER TABLE answers
+ALTER COLUMN created_at TYPE timestamp
+USING TO_TIMESTAMP(created_at / 1000);
