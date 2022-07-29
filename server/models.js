8dea30ab65d274f42e7dc125985e9d957c86de16
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-29 10:33:08 -0700

    fix getAnswers query

--- server/models.js
+++ server/models.js
@@ -51,18 +51,30 @@ module.exports.getQuestions = (product_id, page, count) => {
       client
         .query(`
         SELECT
-        questions.*,
-        json_agg(answers) AS answers
+          questions.*,
+          json_agg(answers) AS answers
         FROM
-        ( SELECT * FROM questions q
-          WHERE q.reported = false AND q.product_id = $1
-          ORDER BY helpfulness DESC
+        ( SELECT
+            *
+          FROM
+            questions q
+          WHERE
+            q.reported = false
+          AND
+            q.product_id = $1
+          ORDER BY
+            helpfulness DESC
           LIMIT $2 OFFSET $3
         ) AS questions
         LEFT JOIN
-        ( SELECT * FROM answers
-          WHERE answers.reported = false
-          ORDER BY helpfulness DESC
+        ( SELECT
+            *
+          FROM
+            answers
+          WHERE
+            answers.reported = false
+          ORDER BY
+            helpfulness DESC
         ) AS answers
         ON
           answers.question_id = questions.id
@@ -80,7 +92,7 @@ module.exports.getQuestions = (product_id, page, count) => {
         `, [product_id, count, (page - 1) * count])
         .then((res) => {
           client.release();
-          console.log(res.rows[0]);
+          console.log(res.rows);
         })
         .catch((err) => {
           client.release();
@@ -98,20 +110,67 @@ module.exports.getQuestions = (product_id, page, count) => {
 // query parameters:
 // - page: integer (default 1)
 // - count: integer (default 5)
+
+// COALESCE(json_agg(a_p.*) FILTER (WHERE a_p.id IS NOT NULL), '[]') AS photos
+
+// json_agg((SELECT rows FROM (SELECT a_p.id, a_p.url) AS rows)) AS photos
 module.exports.getAnswers = (question_id, page, count) => {
   pool
     .connect()
     .then((client) => (
       client
         .query(`
-        SELECT * FROM answers
-        WHERE reported = false AND question_id = $1
-        ORDER BY helpfulness DESC
-        LIMIT $2 OFFSET $3
+        SELECT
+          a.id AS answer_id,
+          a.body,
+          a.created_at AS date,
+          a.username AS answerer_name,
+          a.helpfulness,
+          COALESCE(json_agg(
+            (SELECT rows FROM (SELECT a_p.id, a_p.url) AS rows)
+          ) FILTER (WHERE a_p.id IS NOT NULL), '[]') AS photos
+        FROM
+        ( SELECT
+            *
+          FROM
+            answers
+          WHERE
+            reported = false
+          AND
+            question_id = $1
+          ORDER BY
+            helpfulness DESC
+          LIMIT $2 OFFSET $3
+        ) AS a
+        LEFT JOIN
+        ( SELECT
+           *
+          FROM
+            answers_photos
+        ) AS a_p
+        ON
+          a.id = a_p.answer_id
+        GROUP BY
+          a.id,
+          a.body,
+          a.created_at,
+          a.username,
+          a.helpfulness
+        ORDER BY
+          a.helpfulness DESC
         `, [question_id, count, (page - 1) * count])
         .then((res) => {
           client.release();
-          console.log(res.rows);
+          const response = {
+            question: question_id.toString(),
+            page,
+            count,
+            results: res.rows,
+          };
+          console.log(response);
+          console.log('\n\n\n\n\n1:', res.rows[0]);
+          console.log('\n\n\n\n\n2:', res.rows[1]);
+          console.log('\n\n\n\n\n3:', res.rows[2]);
         })
         .catch((err) => {
           client.release();
@@ -261,4 +320,4 @@ module.exports.reportAnswer = (answer_id) => {
     ));
 };
 // question_id, body, name, email, photos
-console.log(module.exports.getQuestions(1, 1, 3));
+console.log(module.exports.getAnswers(1, 1, 3));
