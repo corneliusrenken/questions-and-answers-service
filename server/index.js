3501ed99195baefc5ca0a1a7cb10631f003f5481
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-08 08:14:23 -0700

    newrelic init

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 
 const port = 6246;
 
 const app = express();
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 app.use(express.json());
 
 app.get('/loader*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'loader.txt')); });
